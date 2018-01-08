6d1f8642bffc341d09c0b98b7144766bfd805801
Nathan <navarrogriffin@gmail.com>
2018-01-07 21:21:26 -0800

    Fixed collision issue with gun drops

---
+++ Assets/Scripts/IgnorePlayerCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnorePlayerCollision : MonoBehaviour {
+
+    public Collider us;
+
+	void Start () {
+        Physics.IgnoreCollision(us, GameObject.FindGameObjectWithTag("Player").transform.GetChild(0).GetComponent<Collider>());
+        this.enabled = false;
+	}
+}
